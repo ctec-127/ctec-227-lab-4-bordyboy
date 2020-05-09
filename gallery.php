a1424a72f054db6b614394336f136a2025ba74d2
Bogdan Livadaru <bogdanlivadaru@yahoo.com>
2020-05-09 16:55:46 -0700

    fixed chrome

--- gallery.php
+++ gallery.php
@@ -5,9 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Image Gallery</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/58eadb60b2.js" crossorigin="anonymous"></script>
 </head>
 <!-- <span class=\"badge badge-pill badge-dark p-0\"><i class=\"fas fa-times-circle fa-2x text-danger\"></i></span> -->
@@ -27,7 +29,7 @@
                         // echo "<div class=\"text-center col-3\"><div class=\"pic mb-5\"><img class=\"img-fluid\" src=\"uploads/$filename\" alt=\"A photo\"></div>";
                         echo "
                         <div class=\"card mr-3 mt-3\" >
-                            <img class=\"card-img-top\" src=\"uploads/$filename\" alt=\"Card image cap\">
+                            <img class=\"\" src=\"uploads/$filename\" alt=\"Card image cap\">
                             <div>                          
                                 <a href=\"gallery.php?file=$filename\" class=\"deleteImage\">
                                     <i class=\"fas fa-times fa-2x text-danger\"></i>                        
